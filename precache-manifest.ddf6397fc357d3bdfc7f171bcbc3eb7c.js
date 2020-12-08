b3b44dbe075ca815c4c1ff53b921e9c3219f1cc0
William Gomes <williamjog@hotmail.com>
2020-12-08 14:50:11 -0300

    Updates

---
+++ precache-manifest.ddf6397fc357d3bdfc7f171bcbc3eb7c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0485cf3d55caaacbc793c500aa9b263b",
+    "url": "/Aplicativo-de-Receitas/index.html"
+  },
+  {
+    "revision": "a2a11f634db1a41b65ab",
+    "url": "/Aplicativo-de-Receitas/static/css/main.d3160300.chunk.css"
+  },
+  {
+    "revision": "7cfd8cfae4f69cc73957",
+    "url": "/Aplicativo-de-Receitas/static/js/2.c8834c9d.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/Aplicativo-de-Receitas/static/js/2.c8834c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a11f634db1a41b65ab",
+    "url": "/Aplicativo-de-Receitas/static/js/main.5e3e7972.chunk.js"
+  },
+  {
+    "revision": "a87a6cb0ae775434637e",
+    "url": "/Aplicativo-de-Receitas/static/js/runtime-main.69a9bfec.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/Aplicativo-de-Receitas/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "c48f87a199c2b3fdbe2bda36a4b15f4e",
+    "url": "/Aplicativo-de-Receitas/static/media/clipart-cookies-cooking-7-original.c48f87a1.png"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/Aplicativo-de-Receitas/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/Aplicativo-de-Receitas/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/Aplicativo-de-Receitas/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/Aplicativo-de-Receitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/Aplicativo-de-Receitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/Aplicativo-de-Receitas/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/Aplicativo-de-Receitas/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
